327a3aa7175190a64015d67e14022742f400e16f
KSN99 <53466034+KSN99@users.noreply.github.com>
2021-12-06 21:40:33 +0900

    유지보수

---
+++ colors.js
@@ -0,0 +1,37 @@
+var Links ={
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+      alist[i].style.color = color;
+      i = i+1;
+  }
+}
+}
+var body = {
+setColor:function (color){
+document.querySelector('body').style.color = color;
+},
+setBackgroundColor:function (color){
+document.querySelector('body').style.backgroundColor = color; // document<<객체
+// querySelector 는 객체에 속해있는 함수 즉, 메소드 (method)라고한다.
+}
+}
+
+
+function nightDayHandler(self){
+var target = document.querySelector('body');
+  if(self.value==='night'){
+  body.setBackgroundColor('black');
+  body.setColor('white');
+  self.value = 'day';
+
+  Links.setColor('blue');
+
+}else {
+    body.setBackgroundColor('white');
+    body.setColor('black'); // 객체 폴더 관점으로 보기 .
+    self.value = 'night';
+    Links.SetColor('black');
+}
+}
